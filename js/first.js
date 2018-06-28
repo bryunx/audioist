8c7bf1c945c4f66ff5f0ebd1ae51f1ac53f9a628
bryunx <bjlfrey@gmail.com>
2018-06-27 23:47:11 -0700

    psd

---
+++ js/first.js
@@ -0,0 +1,9 @@
+var word = 'caliia'
+
+var vowels = /[aeiou]/ig;
+
+var newVowels = word.match(vowels).length;
+console.log(newVowels);
+   
+// keep this function call here 
+// console.log(duplicate());
